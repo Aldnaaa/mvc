65f2f72f14dff70e2caccf246ec7e29ea7fec5ae
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2023-12-19 07:53:29 +0700

    config

--- app/Config/config.php
+++ app/Config/config.php
@@ -6,6 +6,7 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'web_kantin');
-
+// Menambahkan setting timezone
+date_default_timezone_set("Asia/Jakarta");
 
 
