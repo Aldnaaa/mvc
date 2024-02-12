1ec7becb25cfe1b94f1b644f9f07171cc9c98a35
Muhammad Ainur Riziq <riziqainur13@gmail.com>
2024-02-12 14:34:21 +0700

    first commit

--- app/Config/config.php
+++ app/Config/config.php
@@ -9,4 +9,7 @@ define('DB_NAME', 'web_kantin');
 // Menambahkan setting timezone
 date_default_timezone_set("Asia/Jakarta");
 
+// username : admin  , password: admin
+// username: pemilik , password: pemilik
+
 
